6f8477d8c0ab3515e7017c390fd1e3839beb8ed8
Brandon Hamilton <bham2015@gmail.com>
2024-10-15 19:36:50 -0500

    Seeded database with dummy data.

---
+++ prisma/seed.js
@@ -0,0 +1,36 @@
+const prisma = require("./index");
+const { faker } = require("@faker-js/faker");
+
+const seed = async (numUsers = 5, numTracks = 20, numPlaylists = 10) => {
+  const users = Array.from({ length: numUsers }, () => ({
+    username: faker.person.zodiacSign(),
+  }));
+  await prisma.user.createMany({ data: users });
+
+  const tracks = Array.from({ length: numTracks }, () => ({
+    name: faker.music.songName(),
+  }));
+  await prisma.track.createMany({ data: tracks });
+
+  for (let i = 0; i < numPlaylists; i++) {
+    const playlistTracks = Array.from({ length: 8 }, () => ({
+      id: Math.random(Math.floor() * numTracks) + 1,
+    }));
+    await prisma.playlist.create({
+      data: {
+        name: faker.music.album(),
+        description: `Rave Day ${i + 1}`,
+        ownerId: Math.floor(Math.random() * numUsers) + 1,
+        tracks: { connect: playlistTracks },
+      },
+    });
+  }
+};
+
+seed()
+  .then(async () => await prisma.$disconnect())
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
