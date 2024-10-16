f566d1f3ba41ad91bf247a8dcc23f60e1af7d851
Brandon Hamilton <bham2015@gmail.com>
2024-10-15 19:46:05 -0500

    Set up server.js file.

--- server.js
+++ server.js
@@ -2,4 +2,21 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.use(require("morgan")("dev"));
 app.use(express.json());
+
+app.use("/users", require("../Unit4.Jukebox/api/playlist"));
+
+app.use((req, res, next) => {
+  next({ status: 404, message: "Endpoint not found :(." });
+});
+
+app.use((err, req, res, next) => {
+  console.error(error);
+  res.status(err.status ?? 500);
+  res.json(err.message ?? "Sorry, something broke.");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
